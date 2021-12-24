0731b2597a3ca1b9a8298ed03a81ea3c997411a8
devHikary <wrkkarina@gmail.com>
2021-12-24 06:40:02 -0300

    Exibindo o progresso de upload

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/core/user/user.service';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'ap-photo-form',
@@ -15,6 +17,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: string;
+  percentDone = 0;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -22,7 +25,7 @@ export class PhotoFormComponent implements OnInit {
     private photoService: PhotoService,
     private alertService: AlertService,
     private userService: UserService
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.photoForm = this.formBuilder.group({
@@ -35,25 +38,30 @@ export class PhotoFormComponent implements OnInit {
   upload() {
     const description = this.photoForm.get('description').value;
     const allowComments = this.photoForm.get('allowComments').value;
-
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => {
-        this.alertService.success('Upload complete', true);
-        this.router.navigate(['/user', this.userService.getUserName()]);
+      .pipe(finalize(() =>
+        this.router.navigate(['/user', this.userService.getUserName()])
+      ))
+      .subscribe((event: HttpEvent<any>) => {
+        if (event.type == HttpEventType.UploadProgress) {
+          this.percentDone = Math.round(100 * event.loaded / event.total);
+        } else if (event.type == HttpEventType.Response) {
+          this.alertService.success('Upload complete', true);
+        }
       },
-      err => {
-        console.error(err);
-        this.alertService.warning('Upload error')
-      }
-      )
+        err => {
+          console.log(err);
+          this.alertService.danger('Upload error!');
+        });
+
   }
 
-  handleFile(file: File){
+  handleFile(file: File) {
     this.file = file
     const reader = new FileReader();
     reader.onload = (event: any) => this.preview = event.target.result;
-    reader.readAsDataURL(file); 
+    reader.readAsDataURL(file);
   }
 
 }
