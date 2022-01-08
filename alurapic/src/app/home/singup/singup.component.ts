7093c0e9a9186550a3a6f4c32531ebfe96eaf9f6
devHikary <wrkkarina@gmail.com>
2022-01-07 21:20:39 -0300

    Validando na submissão

--- alurapic/src/app/home/singup/singup.component.ts
+++ alurapic/src/app/home/singup/singup.component.ts
@@ -8,65 +8,68 @@ import { Router } from '@angular/router';
 import { PlatformDetectorService } from '../../core/plataform-detector/platform-detector.service';
 
 @Component({
-    templateUrl: './signup.component.html',
-    providers: [ UserNotTakenValidatorService ]
+  templateUrl: './signup.component.html',
+  providers: [UserNotTakenValidatorService]
 })
 export class SignUpComponent implements OnInit {
-    
-    signupForm: FormGroup;
-    @ViewChild('emailInput') emailInput: ElementRef<HTMLInputElement>;
-    
-    constructor(
-        private formBuilder: FormBuilder,
-        private userNotTakenValidatorService: UserNotTakenValidatorService,
-        private signUpService: SignUpService,
-        private router: Router,
-        private platformDetectorService: PlatformDetectorService) {}
 
-    ngOnInit(): void {
-        this.signupForm = this.formBuilder.group({
-            email: ['', 
-                [
-                    Validators.required,
-                    Validators.email
-                ]
-            ],
-            fullName: ['', 
-                [
-                    Validators.required,
-                    Validators.minLength(2),
-                    Validators.maxLength(40)
-                ]
-            ],
-            userName: ['', 
-                [
-                    Validators.required,
-                    lowerCaseValidator,
-                    Validators.minLength(2),
-                    Validators.maxLength(30)
-                ],
-                this.userNotTakenValidatorService.checkUserNameTaken()
-            ],
-            password: ['', 
-                [
-                    Validators.required,
-                    Validators.minLength(8),
-                    Validators.maxLength(14)
-                ]
-            ]
-        });
+  signupForm: FormGroup;
+  @ViewChild('emailInput') emailInput: ElementRef<HTMLInputElement>;
 
-        this.platformDetectorService.isPlatformBrowser() && 
-            this.emailInput.nativeElement.focus();    
-    } 
+  constructor(
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService,
+    private signUpService: SignUpService,
+    private router: Router,
+    private platformDetectorService: PlatformDetectorService) { }
 
-    signup() {
-        const newUser = this.signupForm.getRawValue() as NewUser;
-        this.signUpService
-            .signup(newUser)
-            .subscribe(
-                () => this.router.navigate(['']),
-                err => console.log(err)
-            );
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      userName: ['',
+        [
+          Validators.required,
+          lowerCaseValidator,
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ],
+        this.userNotTakenValidatorService.checkUserNameTaken()
+      ],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14)
+        ]
+      ]
+    });
+
+    this.platformDetectorService.isPlatformBrowser() &&
+      this.emailInput.nativeElement.focus();
+  }
+
+  signup() {
+    if (this.signupForm.valid && !this.signupForm.pending) {
+      const newUser = this.signupForm.getRawValue() as NewUser;
+      this.signUpService
+        .signup(newUser)
+        .subscribe(
+          () => this.router.navigate(['']),
+          err => console.log(err)
+        );
     }
+  }
+
 }
